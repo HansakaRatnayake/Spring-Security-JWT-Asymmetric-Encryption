25f675b8381b917f37b1eb720d75c12702b921d0
hansakarathnayake <hndk2002@gmail.com>
2025-11-20 23:43:53 +0530

    feat: create UserRepository

---
+++ spring-security-asymmetric-encryption/src/main/java/com/lezord/app/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.lezord.app.repository;
+
+import com.lezord.app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    boolean existsByEmailIgnoreCase(String email);
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    boolean existsByPhoneNumber(String phoneNumber);
+}
