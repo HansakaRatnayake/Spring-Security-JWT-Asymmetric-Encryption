24ce5cd0d0aefc3bc07955309c07257b87b775f4
hansakarathnayake <hndk2002@gmail.com>
2025-11-20 23:42:46 +0530

    feat: create Role entity

---
+++ spring-security-asymmetric-encryption/src/main/java/com/lezord/app/entity/Role.java
@@ -0,0 +1,27 @@
+package com.lezord.app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import util.BaseEntity;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ROLE")
+public class Role extends BaseEntity {
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+}
